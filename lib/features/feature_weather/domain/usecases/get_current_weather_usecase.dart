aa69a1d15de54227db9584aeed97a4d93156f7dc
sajjad <sajjad.flutter@gmail.com>
2023-02-14 18:26:43 +0330

    GetCurrentWeatherUseCase added to usecases file

---
+++ lib/features/feature_weather/domain/usecases/get_current_weather_usecase.dart
@@ -0,0 +1,15 @@
+import 'package:weather_app/core/resources/data_state.dart';
+import 'package:weather_app/core/usecase/use_case.dart';
+import 'package:weather_app/features/feature_weather/domain/entities/current_city_entity.dart';
+import 'package:weather_app/features/feature_weather/domain/repository/weather_repository.dart';
+
+class GetCurrentWeatherUseCase
+    extends UseCase<DataState<CurrentCityEntity>, String> {
+  final WeatherRepository _weatherRepository;
+
+  GetCurrentWeatherUseCase(this._weatherRepository);
+  @override
+  Future<DataState<CurrentCityEntity>> call(String param) {
+    return _weatherRepository.fetchCurrentWeatherData(param);
+  }
+}
